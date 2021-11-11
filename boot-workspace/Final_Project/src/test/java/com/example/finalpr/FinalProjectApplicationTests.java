0cac88dc6118a0d8765721ab48bceddc6eb85ee4
hmy343 <hmy343@gmail.com>
2021-11-11 11:03:51 +0900

    boot base
    spring boot base file

---
+++ boot-workspace/Final_Project/src/test/java/com/example/finalpr/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
